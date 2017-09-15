536438780f1f64c203aa1fb24334167569c2668e
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-09-15 23:03:21 +0530

    Re-initialize the android app.

--- Beacona_App/index.android.js
+++ Beacona_App/index.android.js
@@ -1,53 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import { Container, Header, Content, Button, Icon, Text } from 'native-base';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View
 } from 'react-native';
+
+import LoginView from './app/index';
 
 export default class Beacona_App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Container>
+        <Header />
+        <Content>
+          <Button iconLeft light>
+            <Icon name='arrow-back' />
+            <Text>Back</Text>
+          </Button>
+          <Button iconRight light>
+            <Text>Next</Text>
+            <Icon name='arrow-forward' />
+          </Button>
+          <Button iconLeft>
+            <Icon name='home' />
+            <Text>Home</Text>
+          </Button>
+          <Button iconLeft transparent primary>
+            <Icon name='beer' />
+            <Text>Pub</Text>
+          </Button>
+          <Button iconLeft dark>
+            <Icon name='cog' />
+            <Text>Settings</Text>
+          </Button>
+        </Content>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#F5FCFF',
+  // },
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10,
+  // },
+  // instructions: {
+  //   textAlign: 'center',
+  //   color: '#333333',
+  //   marginBottom: 5,
+  // },
 });
 
 AppRegistry.registerComponent('Beacona_App', () => Beacona_App);
