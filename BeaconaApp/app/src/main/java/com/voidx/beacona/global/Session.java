6bf94d09471a7ff2af9e86530a903d9d9fbedcaf
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-09-16 03:38:27 +0530

    no message

--- BeaconaApp/app/src/main/java/com/voidx/beacona/global/Session.java
+++ BeaconaApp/app/src/main/java/com/voidx/beacona/global/Session.java
@@ -19,7 +19,7 @@ public class Session {
         Item sumedheItem = new Item(2, "Sumedhe", "", "Dissanayake", lastCurrentLocation.getLatitude() + 0.0004, lastCurrentLocation.getLongitude() + 0.0028, "");
         Item jinadiItem = new Item(3, "Jinadi", "", "Yasiruka", lastCurrentLocation.getLatitude() + 0.0009, lastCurrentLocation.getLongitude() + 0.0050, "");
         Item kavindaItem = new Item(4, "Kavinda", "", "Niroshan", lastCurrentLocation.getLatitude() + 0.0051, lastCurrentLocation.getLongitude() + 0.0005, "");
-        Item supulItem = new Item(5, "Supul", "", "Dulanka", lastCurrentLocation.getLatitude() - 0.0030, lastCurrentLocation.getLongitude() + 0.0004, "");
+        Item supulItem = new Item(5, "Supul", "", "Dulanka", 6.918281, 79.862596, "");
 
         itemHashMap.put(oshaniItem.id, oshaniItem);
         itemHashMap.put(sumedheItem.id, sumedheItem);
