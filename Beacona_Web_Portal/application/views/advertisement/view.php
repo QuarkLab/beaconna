0094acc8cfad815e19d4baac4b6d938136a34246
apramodya <pramodyaabeysinghe@gmail.com>
2017-11-11 04:25:14 +0530

    Add advertisement model

--- Beacona_Web_Portal/application/views/advertisement/view.php
+++ Beacona_Web_Portal/application/views/advertisement/view.php
@@ -8,9 +8,10 @@
                         <p class="category">View advertisements added from your shop</p>
                     </div>
                     <br>
+                    <?php foreach ($ad as $a): ?>
                     <div class="jumbotron">
                         <div class="row">
-                            <h3 >Ad 1</h3>
+                            <h3 ><?php echo $a['name'];?></h3>
                         </div>
                         <hr>
                         <div class="row">
@@ -19,26 +20,10 @@
                             </div>
                             <div class="col-md-1"></div>
                             <div class="col-md-8">
-                                <p class="text-justify" style="font-size: medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias, aliquid beatae exercitationem facilis illo impedit laborum nobis porro sed. Dolores id, incidunt iste itaque officia reiciendis repellat reprehenderit veritatis?</p>
+                                <p class="text-justify" style="font-size: medium"><?php echo $a['description'];?></p>
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary pull-right">Edit</button>
                         <br>
                     </div>
-                    <div class="jumbotron">
-                        <div class="row">
-                            <h3>Ad 2</h3>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-md-3">
-                                <img style="height:100px; width:200px" src="<?php echo base_url();?>assets/img/logo.png" />
-                            </div>
-                            <div class="col-md-1"></div>
-                            <div class="col-md-8">
-                                <p class="text-justify" style="font-size: medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias, aliquid beatae exercitationem facilis illo impedit laborum nobis porro sed. Dolores id, incidunt iste itaque officia reiciendis repellat reprehenderit veritatis?</p>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary pull-right">Edit</button>
-                        <br>
-                    </div>
+                    <?php endforeach; ?>
