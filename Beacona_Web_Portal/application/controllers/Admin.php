3242087cd7ef01c1d70764ecb8a401dfa40584e4
apramodya <pramodyaabeysinghe@gmail.com>
2017-11-10 19:17:21 +0530

    Add Admin controller

---
+++ Beacona_Web_Portal/application/controllers/Admin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: apramodya
+ * Date: 11/10/17
+ * Time: 7:11 PM
+ */
+class Admin extends CI_Controller {
+    public function index(){
+        $this->load->view('admin/index');
+    }
+}
---
+++ Beacona_Web_Portal/application/views/admin/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apramodya
+ * Date: 11/10/17
+ * Time: 7:12 PM
+ */
+?>
+
