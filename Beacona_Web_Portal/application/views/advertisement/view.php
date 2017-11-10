f9911b9dc64339021a51b0a437aee25454dc72b8
apramodya <pramodyaabeysinghe@gmail.com>
2017-11-10 23:46:41 +0530

    Add advertisement

---
+++ Beacona_Web_Portal/application/views/advertisement/view.php
@@ -0,0 +1,44 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="col-md-12">
+                <div class="">
+                    <div class="card-header" data-background-color="purple">
+                        <h4 class="title">View Advertisements</h4>
+                        <p class="category">View advertisements added from your shop</p>
+                    </div>
+                    <br>
+                    <div class="jumbotron">
+                        <div class="row">
+                            <h3 >Ad 1</h3>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img style="height:100px; width:200px" src="<?php echo base_url();?>assets/img/logo.png" />
+                            </div>
+                            <div class="col-md-1"></div>
+                            <div class="col-md-8">
+                                <p class="text-justify" style="font-size: medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias, aliquid beatae exercitationem facilis illo impedit laborum nobis porro sed. Dolores id, incidunt iste itaque officia reiciendis repellat reprehenderit veritatis?</p>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary pull-right">Edit</button>
+                        <br>
+                    </div>
+                    <div class="jumbotron">
+                        <div class="row">
+                            <h3>Ad 2</h3>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img style="height:100px; width:200px" src="<?php echo base_url();?>assets/img/logo.png" />
+                            </div>
+                            <div class="col-md-1"></div>
+                            <div class="col-md-8">
+                                <p class="text-justify" style="font-size: medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias, aliquid beatae exercitationem facilis illo impedit laborum nobis porro sed. Dolores id, incidunt iste itaque officia reiciendis repellat reprehenderit veritatis?</p>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary pull-right">Edit</button>
+                        <br>
+                    </div>
