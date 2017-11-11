4029909feaa0afa2b750c19b507dedde376ac3f0
Kavinda Jayakody <kavindajayakody@gmail.com>
2017-11-11 07:12:03 +0530

    Edited Settings Page

--- Beacona/src/pages/notifications/notifications.ts
+++ Beacona/src/pages/notifications/notifications.ts
@@ -8,8 +8,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class NotificationsPage {
 
   num = 10;
-  items = [];
-  public notifications: Notification[] = []; 
+  notifications = [
+    {id: 1, name:'Pizza Hut Offers', description:'Pizza Hut Offers! Discounts from 20% for the Credit/Debit card holders',
+     type:'1', price:'Rs. 100.00', shopName: 'PizzaHut', distance: '5km',
+     thumbnail:'assets/imgs/kabajees.png', },
+
+     {id: 1, name:'Carrot', description:'Fresh Vegetables',
+     type:'2', price:'Rs. 150.00 (20% OFF)', shopName: 'Cargills Food City', distance: '5km',
+     thumbnail:'assets/imgs/carrot.png', },
+     
+     {id: 1, name:'Item 3', description:'Description of item 1',
+     type:'3', price:'Rs. 100.00', shopName: 'PizzaHut', distance: '5km',
+     thumbnail:'assets/imgs/clothe.png', },
+
+     {id: 1, name:'Woman Handbag', description:'Odel',
+     type:'2', price:'Rs. 900.00 (Limited)', shopName: 'Odel, Nugegoda', distance: '5km',
+     thumbnail:'assets/imgs/handbag.jpg', },
+    ];
+ 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -23,23 +39,7 @@ export class NotificationsPage {
     // var a:Notification = new Notification('xx');
     // this.num = a.name;
 
-    this.notifications = [
-      {id: 1, name:'Pizza Hut Offers', description:'Pizza Hut Offers! Discounts from 20% for the Credit/Debit card holders',
-       type:'1', price:'Rs. 100.00', shopName: 'PizzaHut', distance: '5km',
-       thumbnail:'assets/imgs/kabajees.png', },
-
-       {id: 1, name:'Carrot', description:'Fresh Vegetables',
-       type:'2', price:'Rs. 150.00 (20% OFF)', shopName: 'Cargills Food City', distance: '5km',
-       thumbnail:'assets/imgs/carrot.png', },
-       
-       {id: 1, name:'Item 3', description:'Description of item 1',
-       type:'3', price:'Rs. 100.00', shopName: 'PizzaHut', distance: '5km',
-       thumbnail:'assets/imgs/clothe.png', },
-
-       {id: 1, name:'Woman Handbag', description:'Odel',
-       type:'2', price:'Rs. 900.00 (Limited)', shopName: 'Odel, Nugegoda', distance: '5km',
-       thumbnail:'assets/imgs/handbag.jpg', },
-      ];
+    
   }
 
   loadNotifications(){
