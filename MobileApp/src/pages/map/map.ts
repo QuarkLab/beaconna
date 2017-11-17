13ed51c2b5ffd703cd97d0956ac725fa0adb1216
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-11-17 20:37:58 +0530

    no message

---
+++ MobileApp/src/pages/map/map.ts
@@ -0,0 +1,86 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  LatLng,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
+ import { Geolocation } from '@ionic-native/geolocation';
+
+ import { MapStyle } from './map.style';
+
+ declare var google: any;
+
+ @IonicPage()
+ @Component({
+   selector: 'page-map',
+   templateUrl: 'map.html',
+ })
+ export class MapPage {
+   @ViewChild('map') mapRef: ElementRef;
+
+   map: any;
+   location: any;
+
+   constructor(public navCtrl: NavController, public navParams: NavParams) {
+   }
+
+   ionViewDidLoad() {
+     this.showMap();
+   }
+
+   showMap(){
+     this.location = new google.maps.LatLng(51.507351, -0.127758);
+
+     //  let watch = this.geolocation.watchPosition();
+     //  watch.subscribe((data) => {
+     //   // data can be a set of coordinates, or an error (if an error occurred).
+     //   // data.coords.latitude
+     //   // data.coords.longitude
+     //  });
+
+     var options = {
+       center: this.location,
+       zoom: 17,
+       streetViewControl: false,
+       mapTypeId: 'roadmap',
+       styles: MapStyle
+     }
+
+     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+
+     this.addMarker(this.location, this.map);
+
+    //  this.geolocation.getCurrentPosition().then((resp) => {
+    //   if(this.isNumeric(resp.coords.latitude) && this.isNumeric(resp.coords.longitude)){
+    //     this.location = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+
+    //     var options = {
+    //       center: this.location,
+    //       zoom: 16,
+    //       streetViewControl: false,
+    //       mapTypeId: 'roadmap'
+    //     }
+
+    //     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    //   }
+    //  }).catch((error) => {
+    //    console.log('Error getting location', error);
+    //  });
+   }
+
+   addMarker(position, map){
+     return new google.maps.Marker({
+       position,
+       map
+     });
+   }
+
+   isNumeric(n: any) : n is number | string {
+     return !isNaN(parseFloat(n)) && isFinite(n);
+   }
+ }
