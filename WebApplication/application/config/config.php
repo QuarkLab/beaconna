c7c4d9863ea1ffe8e302772171bad698e14a855c
Kavinda Jayakody <kavindajayakody@gmail.com>
2017-11-23 13:52:57 +0530

    Update the Elastic IP of the AWS VM

--- WebApplication/application/config/config.php
+++ WebApplication/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://184.72.164.154/beacona';
+$config['base_url'] = 'http://34.238.149.235/beacona';
 
 /*
 |--------------------------------------------------------------------------
